129a75ad21c8709e5323123b1f2f4b99ae47e813
Jason Kurian <jason.kurian@shopify.com>
2022-12-02 01:19:02 -0500

    fix running tests from root

--- shared/download-input.js
+++ shared/download-input.js
@@ -1,9 +1,11 @@
-import { basename, dirname } from "node:path";
-import { writeFileSync, readFileSync, existsSync } from "node:fs";
+import { existsSync, readFileSync, writeFileSync } from "node:fs";
+import { basename, dirname, join } from "node:path";
+
+const __dirname = dirname(new URL(import.meta.url).pathname);
 
 export async function input(fileUrl, year = basename(dirname(fileUrl))) {
   const day = basename(fileUrl).replace(".js", "");
-  const filePath = `../${year}/${day}.txt`;
+  const filePath = join(__dirname, `../${year}/${day}.txt`);
   if (existsSync(filePath)) {
     return readFileSync(filePath).toString();
   }
