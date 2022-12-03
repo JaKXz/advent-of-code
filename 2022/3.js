799feedc79aeb43b8413c36b61d095328695ad0f
Jason Kurian <jason.kurian@shopify.com>
2022-12-03 12:58:52 -0500

    parameterize day 3 part 2, at the cost of perf
    with everything properly named and extracted, the overkill use of generating a range and using it in the predicate slows things down quite a bit but since we're still in µs I think it's acceptable.

--- 2022/3.js
+++ 2022/3.js
@@ -2,6 +2,7 @@ import { test } from "uvu";
 import * as assert from "uvu/assert";
 
 import { input } from "../shared/download-input.js";
+import { range } from "../shared/range.js";
 import { time } from "../shared/timer.js";
 
 const data = await input(import.meta.url);
@@ -39,31 +40,32 @@ CrZsJsPPZsGzwwsLwLmpwMDw`),
   assert.equal(parseLineItems(), 7821);
 });
 
-const parseRucksacksPerThree = (d = data) => {
-  const lines = d.split("\n");
+function parseRucksacksPerTeamSize(d = data, teamSize = 3) {
+  const rucksacks = d.split("\n");
+  if (rucksacks.length % teamSize !== 0) console.warn("Uneven team size");
   let acc = 0;
-  for (let i = 0; i < lines.length + 3; i++) {
-    if (i > 0 && i % 3 === 0) {
-      acc += lines[i - 3]
-        .split("")
-        .filter((c) => lines[i - 2].includes(c) && lines[i - 1].includes(c))
-        .reduce(
-          (sum, c, j, a) =>
-            a.indexOf(c) !== j
-              ? sum
-              : c.toLowerCase() === c
-              ? sum + c.charCodeAt(0) - 96
-              : sum + c.charCodeAt(0) - 38,
-          0
-        );
-    }
+  for (let i = rucksacks.length; i >= teamSize; i -= teamSize) {
+    acc += rucksacks[i - 1]
+      .split("")
+      .filter((badge) =>
+        range(2, teamSize).every((j) => rucksacks[i - j].includes(badge))
+      )
+      .reduce(prioritySummation, 0);
   }
   return acc;
-};
+}
+
+function prioritySummation(sum, badge, i, badges) {
+  return badges.indexOf(badge) !== i
+    ? sum
+    : badge.toLowerCase() === badge
+    ? sum + badge.charCodeAt(0) - 96
+    : sum + badge.charCodeAt(0) - 38;
+}
 
 test("rucksacks per 3", () => {
   assert.equal(
-    parseRucksacksPerThree(`vJrwpWtwJgWrhcsFMMfFFhFp
+    parseRucksacksPerTeamSize(`vJrwpWtwJgWrhcsFMMfFFhFp
 jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
 PmmdzqPrVvPwwTWBwg
 wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
@@ -71,12 +73,12 @@ ttgJtRGJQctTZtZT
 CrZsJsPPZsGzwwsLwLmpwMDw`),
     70
   );
-  assert.equal(parseRucksacksPerThree(), 2752);
+  assert.equal(parseRucksacksPerTeamSize(), 2752);
 });
 
 test("is fast", async () => {
   console.log(await time(parseLineItems));
-  console.log(await time(parseRucksacksPerThree));
+  console.log(await time(parseRucksacksPerTeamSize));
 });
 
 test.run();
