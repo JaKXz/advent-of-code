72a52eea37385e6930ff9c3a0a24ea3d161f8974
Jason Kurian <jason.kurian@shopify.com>
2022-12-06 23:59:03 -0500

    clean up day 6 a bit
    from my non-scientific testing on node 19 at time of writing, solving this problem with array operations is faster than using a Set. don't ask me why :shrug:

--- 2022/6.js
+++ 2022/6.js
@@ -6,48 +6,51 @@ import { time } from "../shared/timer.js";
 
 const data = await input(import.meta.url);
 
-function parseSignal(d = data, distinct = 4) {
+function parseSignalStartLocation(d = data, distinct = 4) {
   const chars = d.split("");
-  let start = 0;
-  for (let end = distinct; end < chars.length; end++) {
-    const buffer = chars.slice(start, end);
-    const len = buffer.length;
-    const unique = buffer.filter((c, i, a) => a.indexOf(c) === i).length;
-    if (unique === len) {
-      return end;
+  for (let i = 0; i < chars.length; i++) {
+    const unique = chars
+      .slice(i, i + distinct)
+      .filter((c, j, a) => a.indexOf(c) === j);
+    if (unique.length === distinct) {
+      return i + distinct;
     }
-    start++;
   }
-  return null;
 }
 
 test("example 1", () => {
-  assert.equal(parseSignal(`mjqjpqmgbljsphdztnvjfqwrcgsmlb`), 7);
+  assert.equal(parseSignalStartLocation(`mjqjpqmgbljsphdztnvjfqwrcgsmlb`), 7);
 });
 test("example 2", () => {
-  assert.equal(parseSignal(`bvwbjplbgvbhsrlpgdmjqwftvncz`), 5);
+  assert.equal(parseSignalStartLocation(`bvwbjplbgvbhsrlpgdmjqwftvncz`), 5);
 });
 test("example 3", () => {
-  assert.equal(parseSignal(`nppdvjthqldpwncqszvftbrmjlhg`), 6);
+  assert.equal(parseSignalStartLocation(`nppdvjthqldpwncqszvftbrmjlhg`), 6);
 });
 test("example 4", () => {
-  assert.equal(parseSignal(`nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg`), 10);
+  assert.equal(
+    parseSignalStartLocation(`nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg`),
+    10
+  );
 });
 test("example 5", () => {
-  assert.equal(parseSignal(`zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw`), 11);
+  assert.equal(
+    parseSignalStartLocation(`zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw`),
+    11
+  );
 });
 
 test("part 1", () => {
-  assert.equal(parseSignal(), 1100);
+  assert.equal(parseSignalStartLocation(), 1100);
 });
 
 test("part 2", () => {
-  assert.equal(parseSignal(data, 14), 2421);
+  assert.equal(parseSignalStartLocation(data, 14), 2421);
 });
 
 test("is fast", async () => {
-  console.log(await time(parseSignal));
-  console.log(await time(parseSignal.bind(null, data, 14)));
+  console.log(await time(parseSignalStartLocation));
+  console.log(await time(parseSignalStartLocation.bind(null, data, 14)));
 });
 
 test.run();
